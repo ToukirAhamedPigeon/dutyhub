aee05f38f6066274eb74bc7ecd111d5ebf4ad3bd
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-06-29 10:39:42 +0600

    Logo Border

--- components/module/admin/layout/Logo.tsx
+++ components/module/admin/layout/Logo.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 export default function Logo({isTitle, className, titleClassName}: {isTitle: boolean, className?: string, titleClassName?: string}){
     return (
         <Link href="/" className={cn('flex items-center gap-3', className)}>
-            <div className='bg-white/80 p-1 rounded-xl'>
+            <div className='bg-white/80 p-[2px] rounded-full'>
                 <Image src="/logo.png" alt="Logo" width={32} height={32}  />
             </div>
             {isTitle && <span className={cn("text-lg font-semibold", titleClassName)}>Duty Hub</span>}
